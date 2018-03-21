4e089cb365eefc9e9ba8687c11ec5323268321a9
Viet Lam <tranvietlam92@gmail.com>
2018-03-21 23:46:03 +0700

    update authen

--- modules/poc-backend-auth/src/main/java/poc/rest/auth/BackendAuth.java
+++ modules/poc-backend-auth/src/main/java/poc/rest/auth/BackendAuth.java
@@ -7,38 +7,6 @@ import com.liferay.portal.kernel.service.ServiceContext;
  */
 public interface BackendAuth {
 	
-//	/**
-//	 * @author khoavu
-//	 * @param context
-//	 * @param modelName
-//	 * @return
-//	 */
-//	public boolean isAdmin(ServiceContext context, String modelName);
-//	
-//	/**
-//	 * @param context
-//	 * @param security
-//	 * @param password
-//	 * @return
-//	 */
-//	public boolean isAuth(ServiceContext context, String security, String password);
-//	
-//	/**
-//	 * @param context
-//	 * @param modelName
-//	 * @param actionId
-//	 * @return
-//	 */
-//	public boolean hasResource(ServiceContext context, String modelName, String actionId);
-//	
-//	/**
-//	 * @param context
-//	 * @param modelName
-//	 * @param actionId
-//	 * @return
-//	 */
-//	public boolean userHasResource(ServiceContext context, String modelName, String actionId);
-
 	/**
 	 * @param context
 	 * @param security
